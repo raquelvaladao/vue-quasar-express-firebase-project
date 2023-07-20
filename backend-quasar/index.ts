4869829cfd4a6b15ede47b5258d070ea3e48b429
raquelvaladao <raquelvaladao@usp.br>
2023-07-20 20:35:57 -0300

    service account

--- backend-quasar/index.ts
+++ backend-quasar/index.ts
@@ -1,19 +1,38 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { fire } from './middlewares/Firebase';
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
 import { Recipe } from './models/Recipe';
 dotenv.config();
 
+
 const app: Express = express();
+const { getFirestore } = require('firebase-admin/firestore');
 const port = process.env.PORT;
-const db = getFirestore(fire)
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 
+app.get('/all', async (req, res) => {
+  try {
+    const db = admin.firestore();
+    const snapshot = await db.collection('food').get();
 
-app.get('/', async (req: Request, res: Response) => {
-  const list = await getFoodList();
-  res.send(list);
+    const foods: any = [];
+    snapshot.forEach((doc: any) => {
+      foods.push(doc.data());
+    });
+
+    res.json(foods);
+  } catch (error) {
+    console.error('Erro ao listar dados da coleção "food":', error);
+    res.status(500).json({ error: 'Erro ao listar dados da coleção "food"' });
+  }
 });
 
 app.get('/test', async (req: Request, res: Response) => {
@@ -24,11 +43,14 @@ app.get('/test', async (req: Request, res: Response) => {
     id: 90629,
     image: 'https://spoonacular.com/recipeImages/90629-312x231.jpg',
     protein: '1g',
-    title: 'Baked Apples in White Wine'
+    title: 'Teste'
   };
   
   //if not
-  addDoc(collection(db, "food"), novoDoc);
+  //adicionar à collection food um alimento com id da API
+  const docRef = db.collection('food').doc(novoDoc.id); 
+
+  await docRef.set(novoDoc);
   res.send(await getFoodList());
 });
 
@@ -38,8 +60,9 @@ app.listen(port, () => {
 });
 
 async function getFoodList() {
-  const coll = collection(db, 'food');
-  const docs = await getDocs(coll);
-  const list = docs.docs.map(doc => doc.data());
-  return list;
+  const snapshot = await db.collection('food').get();
+  snapshot.forEach((doc: any) => {
+    console.log(doc.id, '=>', doc.data());
+  });
+  return "Adicionado!";
 }
