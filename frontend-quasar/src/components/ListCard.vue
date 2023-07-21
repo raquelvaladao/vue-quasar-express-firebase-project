af72427fdcaeca367226a98f0507017f0e0cfc9a
raquelvaladao <raquelvaladao@usp.br>
2023-07-20 23:51:33 -0300

    modal

--- frontend-quasar/src/components/ListCard.vue
+++ frontend-quasar/src/components/ListCard.vue
@@ -1,9 +1,16 @@
 <template>
   <q-page>
+    <q-pagination
+      v-model="currentPage"
+      :min="1"
+      :max="totalPages"
+      @input="changePage"
+      class="pagination"
+    />
     <q-page-container style="padding-top: 0%; padding-left: 0%;">
       <div class="q-pa-md">
         <q-list bordered separator>
-          <q-item v-for="(card, index) in paginatedCards" :key="index">
+          <q-item v-for="(card, index) in paginatedCards" :key="index" class="q-mb-md">
             <q-item-section avatar>
               <q-avatar>
                 <img :src="card.image" alt="Card Image">
@@ -14,28 +21,38 @@
               <q-item-label>{{ card.name }}</q-item-label>
               <q-item-label lines="2">{{ card.description }}</q-item-label>
               <q-item-label>
-                <q-badge label="Badge 1" color="primary" />
-                <q-badge label="Badge 2" color="secondary" />
-                <q-badge label="Badge 3" color="accent" />
+                <q-badge rounded label="55g prot" color="primary" />
+                <q-badge rounded label="35g carb" color="secondary" />
+                <q-badge rounded label="12g gord" color="accent" />
               </q-item-label>
 
-              <!-- Botão de edição -->
-              <q-item-section side>
-                <q-btn flat icon="edit" class="q-ml-auto" @click="editCard(card)"/>
-              </q-item-section>
+              <!-- Nova div para envolver os botões -->
+              <div class="q-ml-auto" style="display: flex;">
+                <q-btn flat icon="edit" @click="editCard(card)" />
+                <q-btn flat icon="favorite" @click="favoriteCard(card)" />
+                <q-btn flat icon="remove_red_eye" @click="viewCard(card)" />
+              </div>
             </q-item-section>
           </q-item>
         </q-list>
       </div>
     </q-page-container>
 
-    <q-pagination
-      v-model="currentPage"
-      :min="1"
-      :max="totalPages"
-      @input="changePage"
-      class="pagination"
-    />
+    <!-- Modal para exibir os dados mockados -->
+    <q-dialog v-model="showModal">
+      <q-card>
+        <q-card-section>
+          <q-item-label>Modal Content</q-item-label>
+          <q-item-label>Name: {{ selectedCard.name }}</q-item-label>
+          <q-item-label>Description: {{ selectedCard.description }}</q-item-label>
+          <!-- Adicione mais informações mockadas conforme necessário -->
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Close" color="primary" @click="showModal = false" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -45,6 +62,8 @@ export default defineComponent({
   name: 'ListCard',
   data () {
     return {
+      showModal: false,
+      selectedCard: {},
       cards: [
         {
           image: 'https://cdn.quasar.dev/img/parallax2.jpg',
@@ -98,10 +117,24 @@ export default defineComponent({
     // Change the current page when the pagination component emits the 'input' event
     changePage (page) {
       this.currentPage = page
+      console.log(this.currentPage)
     },
     editCard (card) {
       // Add your edit card logic here
       console.log('Edit card', card)
+    },
+    favoriteCard (card) {
+      // Add your favorite card logic here
+      console.log('Favorite card', card)
+    },
+    viewCard (card) {
+      // Abrir o modal e preencher os dados mockados
+      this.selectedCard = {
+        name: card.name,
+        description: card.description
+        // Adicione mais informações mockadas conforme necessário
+      }
+      this.showModal = true
     }
   }
 })
@@ -109,14 +142,24 @@ export default defineComponent({
 
 <style>
 /* Add your custom styles for the card here */
-.my-card-list {
-  max-height: calc(100vh - 100px); /* 100px for padding, you can adjust as needed */
-  overflow-y: auto;
+.q-page-container {
+  padding: 0 !important;
+  margin: 0 !important;
 }
 
-.my-container {
-  padding-top: 0%;
-  padding-left: 0%;
+/* Center the content inside q-page */
+.q-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto  ;
+  padding-top: 2%;
+}
+
+/* Optional: Add some margin at the top and bottom of q-list */
+.q-pa-md {
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 
 .pagination {
