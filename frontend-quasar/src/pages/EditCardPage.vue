ccd017fab27d832241398fb0b43805f7fa614a76
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-07-24 14:03:09 -0300

    ajuste id edicao

--- frontend-quasar/src/pages/EditCardPage.vue
+++ frontend-quasar/src/pages/EditCardPage.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md" style="justify-content: center; align-items: center; padding: 20px 150px 0 150px">
     <q-form class="q-gutter-md" @submit="onSubmit" @reset="onReset">
       <q-input filled v-for="(item, index) in dataSelected" :key="index.toString()"
-        :hint="index.toUpperCase()" lazy-rules :rules="[ val => val && val.length > 0 || 'Digite algo!']" v-model="dataSelected[index]" />
+        :hint="index.toUpperCase()" lazy-rules :rules="[ val => val && val.length > 0 || 'Digite algo!']" v-model="dataSelected[index]" :value="item" />
 
       <div>
         <q-btn label="Submit" type="submit" color="primary" />
@@ -23,7 +23,7 @@ export default defineComponent({
       selectedCard: {}, // Change this to an object instead of an array
       dataSelected: {
         title: '',
-        calories: -1,
+        calories: 0,
         protein: '',
         fat: '',
         carbs: ''
@@ -42,7 +42,6 @@ export default defineComponent({
         for (const item of response) {
           if (item.id === Number(id)) {
             this.selectedCard = item
-            this.dataSelected.id = item.id
             this.dataSelected.title = item.title
             this.dataSelected.calories = item.calories
             this.dataSelected.protein = item.protein
@@ -56,11 +55,7 @@ export default defineComponent({
       }
     },
     onReset () {
-      this.formData.title = null
-      this.formData.calories = null
-      this.formData.protein = null
-      this.formData.fat = null
-      this.formData.carbs = null
+      this.loadDetail(this.$route.params.id)
     },
     onSubmit () {
       this.editCard()
