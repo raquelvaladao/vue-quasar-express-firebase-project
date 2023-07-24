606f7342960d7137a222efaf3a1c8d3a9c9b7f55
Matheus <maatheus_98@hotmail.com>
2023-07-24 10:55:35 -0300

    ajuste para mostrar 1o item no carousel

--- frontend-quasar/src/components/CarouselModal.vue
+++ frontend-quasar/src/components/CarouselModal.vue
@@ -4,7 +4,7 @@
             <q-carousel v-model="slide" transition-prev="scale" transition-next="scale" swipeable animated
                 control-color="white" navigation padding arrows height="300px"
                 class="bg-primary text-white shadow-10 rounded-borders">
-                <q-carousel-slide v-for="(item, index) in detailedCard.extendedIngredients" :key="index" :name="item.name + item.id"
+                <q-carousel-slide v-for="(item, index) in detailedCard.extendedIngredients" :key="index" :name="index"
                     class="column no-wrap flex-center">
                     <div class="q-mt-md text-center">
                         {{ item.name }}
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'CarouselModal',
@@ -26,9 +26,9 @@ export default defineComponent({
       required: true
     }
   },
-  setup () {
+  data () {
     return {
-      slide: ref('style')
+      slide: 0
     }
   }
 })
